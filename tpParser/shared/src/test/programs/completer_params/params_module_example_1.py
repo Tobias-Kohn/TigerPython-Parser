41cf47b9e2a9b3b58e3ce73c294b8934f46533c8
Neil Brown <neil@twistedsquare.com>
2024-12-17 17:56:45 +0000

    Added some tests for parameter names in the autocomplete, which already pass.

---
+++ tpParser/shared/src/test/programs/completer_params/params_module_example_1.py
@@ -0,0 +1,13 @@
+# parent1.child1.shape1
+# class Shape:
+#    pos_x
+#    pos_y
+#    __init__(self, x, y)
+#    draw(self)
+#    extent(self)
+#    rotate_by(self, degrees)
+# [Shape]make_shape()
+# 49
+# degrees
+from parent1.child1.shape1 import *
+make_shape().rotate_by()
